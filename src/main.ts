b5519dec33a0068a88db17bd4af7f1a08803236f
victor martinez <vitico841@gmail.com>
2025-01-28 01:55:53 -0400

    Agrega configuración de NATS y un archivo .env.example; actualiza main.ts para usar microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const context = await NestFactory.createApplicationContext(AppModule);
+  const configService = context.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  const NATS_URL = configService.get<string>('NATS_URL');
+  context.close();
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers:[NATS_URL]
+      },
+    },
+  );
+  await app.listen()
 }
 bootstrap();
