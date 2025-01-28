a0c15befc9215e3f22f527a90fa453e8768ec8f2
victor martinez <vitico841@gmail.com>
2025-01-27 21:55:30 -0400

    Agrega @nestjs/config y dotenv-cli como dependencias; configura ConfigModule en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { InventoryModule } from './inventory/inventory.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    InventoryModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      isGlobal: true,
+      
+    }),
+  ],
   controllers: [],
   providers: [],
 })
